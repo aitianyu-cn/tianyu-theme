b1d3ebca3f4effe1b0a3aa42c15372e22227d920
victor yao <victor.yao@sap.com>
2024-02-26 14:10:20 +0800

    init repo
    branch: master
    user: victor.yao

---
+++ index.cjs
@@ -0,0 +1,9 @@
+/**@format */
+
+"use strict";
+
+const path = require("path");
+
+module.exports = {
+  resourcePath: path.join(__dirname, "content"),
+};
---
+++ index.mjs
@@ -0,0 +1,9 @@
+/**@format */
+
+import { fileURLToPath } from "node:url";
+import { dirname, join } from "node:path";
+
+export const resourcePath = join(
+  dirname(fileURLToPath(import.meta.url)),
+  "content"
+);
